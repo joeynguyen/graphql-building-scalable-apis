18c7abe32ccee27fe765e2f99cf0dfafdb32c5f0
Joey Nguyen <joey.nguyen@dealertrack.com>
2017-09-22 18:57:52 -0500

    Fix Eslint warnings

--- schema/index.js
+++ schema/index.js
@@ -1,9 +1,9 @@
 // Import type helpers from graphql-js
 const {
-    GraphQLSchema,
-    GraphQLNonNull,
-    GraphQLObjectType,
-    GraphQLString,
+  GraphQLSchema,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
 } = require('graphql');
 
 const MeType = require('./types/me');
@@ -11,30 +11,32 @@ const MeType = require('./types/me');
 // The root query type is where in the data graph
 // we can start asking questions
 const RootQueryType = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        me: {
-            type: MeType,
-            description: 'The current user identified by an API key',
-            args: {
-                key: { type: new GraphQLNonNull(GraphQLString) }
-            },
-            resolve: () => {
-                return {id: 42, email: 'test@email.com'}
-            }
-        },
-        hello: {
-            type: GraphQLString,
-            description: 'The *mandatory* hello world example, GraphQL style',
-            resolve: () => 'world'
+  name: 'RootQueryType',
+  fields: {
+    me: {
+      type: MeType,
+      description: 'The current user identified by an API key',
+      args: {
+        key: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: () => (
+        {
+          id: 42, email: 'test@email.com',
         }
-    }
+      ),
+    },
+    hello: {
+      type: GraphQLString,
+      description: 'The *mandatory* hello world example, GraphQL style',
+      resolve: () => 'world',
+    },
+  },
 });
 
 // nc == 'name-contests'
 const ncSchema = new GraphQLSchema({
-    query: RootQueryType,
-    // mutation: ...
+  query: RootQueryType,
+  // mutation: ...
 });
 
 module.exports = ncSchema;
